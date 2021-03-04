2ca3cd583e8f9d8777d46c9e126273e8aefc239d
emksolandroid <maturioracle@gmail.com>
2021-03-04 14:36:54 +0100

    Adding the missing files

--- models/projecteditor/Form.kt
+++ models/projecteditor/Form.kt
@@ -8,3 +8,8 @@ fun createFormField(field: Field, i: Int): TemplateFormFieldFiller {
     return TemplateFormFieldFiller(name = field.name.condensePropertyName(), label = field.label
             ?: field.name, viewId = i, isRelation = field.inverseName != null, isImage = field.fieldType == 3)
 }
+
+fun createFormField(customNameWithFormatTemplate: String ,field: Field, i: Int): TemplateFormFieldFiller{
+    return TemplateFormFieldFiller(name = customNameWithFormatTemplate, label = field.label
+        ?: field.name, viewId = i, isRelation = field.inverseName != null, isImage = field.fieldType == 3)
+}
