503d0fd93a45a68f72d3e4c7e3a9321d095a6026
Quentin Marciset <quentin.marciset@4d.com>
2022-11-07 16:41:58 +0100

    clean up

--- utils/Formatters.kt
+++ utils/Formatters.kt
@@ -82,15 +82,6 @@ private fun JSONObject.getDataSource(): Any {
     return dataSource
 }
 
-data class InputControlDataSource(
-    var dataClass: String? = null,
-    var field: String? = null,
-    var entityFormat: String? = null,
-    var search: Any? = null, // Bool, String, Array
-    var order: String? = null,
-    var sort: Any? = null // String, Array (of String, Object (field, order) , Object (field, order)
-)
-
 fun getSize(manifestContent: JSONObject, type: String): Int? =
     manifestContent.getSafeObject("assets")?.getSafeObject("size")?.getSafeInt(type)
         ?: manifestContent.getSafeObject("assets")?.getSafeInt("size")
